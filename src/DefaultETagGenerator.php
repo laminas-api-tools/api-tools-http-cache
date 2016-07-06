99e189c9d7ffbaf8829d0e82eb9467c5f4aa9a18
Matthew Weier O'Phinney <matthew@zend.com>
2016-07-06 12:00:15 -0500

    Updated license to 2016
    - Since these are being introduced this year.
    - Also, removed duplicated docblock in implementation.

--- src/DefaultETagGenerator.php
+++ src/DefaultETagGenerator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
 namespace ZF\HttpCache;
@@ -12,11 +12,7 @@ use Zend\Http\Response as HttpResponse;
 class DefaultETagGenerator implements ETagGeneratorInterface
 {
     /**
-     * Returns an ETag for the given response.
-     *
-     * @param HttpRequest $request
-     * @param HttpResponse $response
-     * @return string Etag
+     * {@inheritDoc}
      */
     public function generate(HttpRequest $request, HttpResponse $response)
     {
--- src/ETagGeneratorInterface.php
+++ src/ETagGeneratorInterface.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
 namespace ZF\HttpCache;
@@ -16,7 +16,7 @@ interface ETagGeneratorInterface
      *
      * @param HttpRequest $request
      * @param HttpResponse $response
-     * @return string Etag
+     * @return string ETag
      */
     public function generate(HttpRequest $request, HttpResponse $response);
 }
